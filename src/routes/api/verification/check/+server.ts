f706f4b62fcf61d09f8cf477c3166483d1344c69
marc. <marc.deike@outlook.de>
2025-12-08 23:57:04 +0100

    switched from json response to plain text

--- src/routes/api/verification/check/+server.ts
+++ src/routes/api/verification/check/+server.ts
@@ -1,4 +1,4 @@
-import { error, json } from "@sveltejs/kit"
+import { error } from "@sveltejs/kit"
 import { _pendingClients } from "../start/+server"
 
 export async function GET({ url }) {
@@ -6,15 +6,17 @@ export async function GET({ url }) {
     if (!_pendingClients.has(uuid))
         error(400, "Provided UUID has no pending verification!")
 
+    console.log("size: " + _pendingClients.size)
+
     const start = Date.now()
-    while (Date.now() - start < 10_000) {
+    while (Date.now() - start < 9_000) {
         if (!_pendingClients.get(uuid)!.complete) {
             // wait 20ms and try again
             await new Promise(r => setTimeout(r, 20))
             continue
         }
-        return json({ complete: true })
+        return new Response("true")
     }
     
-    return json({ complete: false })
+    return new Response("false")
 }
--- src/routes/api/verification/clean/+server.ts
+++ src/routes/api/verification/clean/+server.ts
@@ -2,7 +2,7 @@ import { _pendingClients } from "../start/+server";
 
 export async function GET() {
     for(const uuid in _pendingClients) {
-        if (_pendingClients.get(uuid)!.startTime + 3600 < Date.now())
+        if (_pendingClients.get(uuid)!.startTime + 3_600_000 < Date.now())
             _pendingClients.delete(uuid)
     }
 }
